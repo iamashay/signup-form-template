78939fefc6dead25aed5b4a64cb879418cf6482b
Ashay Jaiswal <ashaywin@gmail.com>
2022-05-12 10:56:42 +0530

    Correct output element to include confirm pass

---
+++ js/validation.js
@@ -0,0 +1,5 @@
+const submitBut = document.querySelector("#submit-but");
+const pwdResult = document.querySelector("#pass-result");
+const pwd = document.querySelector("#pass");
+const confirmPwd = document.querySelector("#confirm-pass");
+
